e64903b0056baedf932a6eb6d5c9a7d181cb0176
vinod9030 <vinod@promactinfo.com>
2022-10-21 14:12:27 +0530

    ecs

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "default" {
-  cidr_block = "10.32.0.0/16"
+resource "aws_vpc" "ecs" {
+  cidr_block = "172.31.0.0/16"
   tags = {
     Name = "Taskvpc"
   }
@@ -7,27 +7,27 @@ resource "aws_vpc" "default" {
 
 resource "aws_subnet" "public" {
   count                   = 2
-  cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
-  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)"
+  availability_zone       = "data.aws_availability_zones.available_zones.names[count.index]"
+  vpc_id                  = "vpc-0382f51c959645e99"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   count             = 2
-  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id            = aws_vpc.default.id
+  cidr_block        = "cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)"
+  availability_zone = "data.aws_availability_zones.available_zones.names[count.index]"
+  vpc_id            = "vpc-0382f51c959645e99"
 }
 
 resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.default.id
+  vpc_id = "vpc-0382f51c959645e99"
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.default.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gateway.id
+  route_table_id         = "rtb-07d1bd9f86996008e"
+  destination_cidr_block = "172.31.0.0/16"
+  gateway_id             = "igw-0d99675bb66becd2c"
 }
 
 resource "aws_eip" "gateway" {
@@ -38,41 +38,41 @@ resource "aws_eip" "gateway" {
 
 resource "aws_nat_gateway" "gateway" {
   count         = 2
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.gateway.*.id, count.index)
+  subnet_id     = "subnet-06d9db5a1de59d5c7"
+  allocation_id = "igw-0d99675bb66becd2c"
 }
 
 resource "aws_route_table" "private" {
   count  = 2
-  vpc_id = aws_vpc.default.id
+  vpc_id = "vpc-0382f51c959645e99"
 
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
+    cidr_block = "172.31.16.0/20"
+    nat_gateway_id = "igw-0d99675bb66becd2c"
   }
 }
 
 resource "aws_route_table_association" "private" {
   count          = 2
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
+  subnet_id      = "subnet-0794f192b8185a46a"
+  route_table_id = "rtb-07d1bd9f86996008e"
 }
 
 resource "aws_security_group" "lb" {
-  name        = "example-alb-security-group"
-  vpc_id      = aws_vpc.default.id
+  name        = "ecs-alb-security-group"
+  vpc_id      = "vpc-0382f51c959645e99"
 
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 
   egress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
