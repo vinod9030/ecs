e401d3831b18ed6da962a964dd660a870a220df2
vinod9030 <vinod@promactinfo.com>
2022-10-21 13:29:41 +0530

    ecs

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,20 +1,20 @@
 resource "aws_ecs_service" "hello_world" {
   name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.hello_world.arn
-  desired_count   = var.app_count
+  cluster         = "arn:aws:ecs:us-east-1:214712740451:cluster/ecs"
+  task_definition = "arn:aws:ecs:us-east-1:214712740451:cluster/ecs"
+  desired_count   = "var.app_count"
   launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups = [aws_security_group.hello_world_task.id]
-    subnets         = aws_subnet.private.*.id
+    security_groups = "sg-0551d0f7731151aa3"
+    subnets         = "subnet-0794f192b8185a46a"
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.hello_world.id
+    target_group_arn = "aws_lb_target_group.hello_world.arn:aws:elasticloadbalancing:us-east-1:214712740451:targetgroup/ecs/e6b42b0588ce541f"
     container_name   = "hello-world-app"
     container_port   = 3000
   }
 
-  depends_on = [aws_lb_listener.hello_world]
+  depends_on = "aws_lb_listener.hello_world"
 }
