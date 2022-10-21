a3f4fd6680b7e76ce079ea5d94d09cb91957e9a4
vinod9030 <vinod@promactinfo.com>
2022-10-21 12:46:30 +0530

    ecs

--- alb.tf
+++ alb.tf
@@ -1,24 +1,24 @@
 resource "aws_lb" "default" {
-  name            = "example-lb"
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.lb.id]
+  name            = "ecs-lb"
+  subnets         = "subnet-06d9db5a1de59d5c7"
+  security_groups = "[sg-0551d0f7731151aa3]"
 }
 
 resource "aws_lb_target_group" "hello_world" {
   name        = "example-target-group"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.default.id
+  vpc_id      = "vpc-0382f51c959645e99"
   target_type = "ip"
 }
 
 resource "aws_lb_listener" "hello_world" {
-  load_balancer_arn = aws_lb.default.id
+  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:214712740451:loadbalancer/net/ecs/fb14ab50529d4118"
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.hello_world.id
+    target_group_arn ="arn:aws:elasticloadbalancing:us-east-1:214712740451:targetgroup/ecs/e6b42b0588ce541f"
     type             = "forward"
   }
 }
